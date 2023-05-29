53a31b0d88ce55f94847bade969bf1859c83aa36
Taif Mahbub Shapnow <103271913+Taif19@users.noreply.github.com>
2023-05-29 21:39:48 +0600

    Delete Integration directory

--- Integration/Double_Int_Python_scipy.py
+++
@@ -1,9 +0,0 @@
-# import scipy.integrate.dblquad
-from scipy import integrate
-  
-f = lambda y, x: x * y**2
-  
-# using scipy.integrate.dblquad() method
-I = integrate.dblquad(f, 0, 3, lambda x: 0, lambda x: 1)
-  
-print(I)
